87d0d55dee6e44842599e67273c2addd49cac92e
Michael McMahon <michael.steven.mcmahon@gmail.com>
2016-09-23 23:57:48 -0400

    Modified additional packages

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 # setup.sh
 # Michael McMahon
+# setup.sh recreates my workshop environment from a clean Raspbian
 
 # Run this file on a Raspberry Pi with this command:
 # sh setup.sh
-# You will be asked for your password.
 
 # Update repository lists
 sudo apt-get update
 
-# Install Synaptic for viewing available programs
-sudo apt-get install -y synaptic
+# Upgrade all packages on the system
+sudo apt-get dist-upgrade -y
 
 # Install GPIO module for python
 sudo apt-get install -y python-dev
@@ -20,35 +20,42 @@ wget https://processing.org/download/install-arm.sh
 sudo sh install-arm.sh
 # sudo aptitude remove libgles1-mesa libgles2-mesa # Uncomment the beginning of this line to enable P3D.
 
-# Install on-screen keyboard for optional touch support
-sudo apt-get install -y matchbox-keyboard
-
-# Upgrade all packages on the system
-sudo apt-get dist-upgrade -y
-
-# Add additional packages
-sudo apt-get install -y feh
-sudo apt-get install -y gimp
-sudo apt-get install -y git
-sudo apt-get install -y telnet
-sudo apt-get install -y tightvncserver
-sudo apt-get install -y xtightvncviewer
-sudo apt-get install -y midori # Lightweight web browser
-sudo apt-get install -y firefox-esr
-# Ascii Art related packages:
-sudo apt-get install -y xaos
-sudo apt-get install -y cowsay
+# Install additional packages
+sudo apt-get install -y awesome # Awesome Window Manager
 sudo apt-get install -y fortune
-sudo apt-get install -y bb
-sudo apt-get install -y libaa-bin
-sudo apt-get install -y caca-utils
+sudo apt-get install -y git
+sudo apt-get install -y matchbox-keyboard # On-screen keyboard for optional touch support
+sudo apt-get install -y synaptic # GUI for apt-get enables scrolling and searching through available programs
+sudo apt-get install -y telnet
+# Ascii Art
 sudo apt-get install -y aview
+sudo apt-get install -y bb
+sudo apt-get install -y caca-utils
+sudo apt-get install -y cowsay
 sudo apt-get install -y figlet
+sudo apt-get install -y libaa-bin
+sudo apt-get install -y screenfetch
 sudo apt-get install -y sl
-sudo apt-get install -y xfonts-mona
-sudo apt-get install -y xfonts-terminus
 sudo apt-get install -y xfonts-100dpi
 sudo apt-get install -y xfonts-75dpi
 sudo apt-get install -y xfonts-base
-sudo apt-get install -y screenfetch
+sudo apt-get install -y xfonts-mona
+sudo apt-get install -y xfonts-terminus
+sudo apt-get install -y xaos
+# Graphic Design
+sudo apt-get install -y feh
+sudo apt-get install -y gimp
+sudo apt-get install -y imagemagick
+# VNC
+sudo apt-get install -y tightvncserver
+sudo apt-get install -y xtightvncviewer
+# Web browsers
+sudo apt-get install -y firefox-esr
+sudo apt-get install -y lynx
+sudo apt-get install -y midori # Lightweight web browser
+sudo apt-get install -y youtube-dl
+sudo apt-get install -y w3m
 
+# Backup python_games
+cd ~
+tar cvf python_games.tar python_games
